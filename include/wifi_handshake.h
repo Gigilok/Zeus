73300ba7136b16daf0e3356e82137485f7f9d62c
Gigilok <cliquefeirago@gmail.com>
2026-07-23 13:17:05 -0300

    Create wifi_handshake.h

---
+++ include/wifi_handshake.h
@@ -0,0 +1,17 @@
+#ifndef WIFI_HANDSHAKE_H
+#define WIFI_HANDSHAKE_H
+
+#include <Arduino.h>
+
+void startHandshakeCapture();
+void stopHandshakeCapture();
+bool isHandshakeCapturing();
+
+const char* getHandshakeStatus();
+uint8_t getHandshakeMessageCount();
+bool isHandshakeComplete();
+
+void sendHandshakeViaBluetooth();
+void clearHandshakeBuffer();
+
+#endif
