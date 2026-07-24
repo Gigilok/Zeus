150b13db8af2df9f4cd687cb816c2b5e2587a3ec
Gigilok <cliquefeirago@gmail.com>
2026-07-24 10:38:33 -0300

    Update wifi_handshake.h

--- include/wifi_handshake.h
+++ include/wifi_handshake.h
@@ -6,10 +6,12 @@
 void startHandshakeCapture();
 void stopHandshakeCapture();
 bool isHandshakeCapturing();
-
 const char* getHandshakeStatus();
 uint8_t getHandshakeMessageCount();
 bool isHandshakeComplete();
 void clearHandshakeBuffer();
+
+// Nova função usando buffer binário para não corromper o arquivo PCAP
+uint8_t* getPcapData(size_t* outLen);
 
 #endif
